e99da51cba70281667d35d73a6b9ac76d2782c64
vladkorosko <54815768+vladkorosko@users.noreply.github.com>
2020-01-12 16:21:19 +0200

    Add files via upload

---
+++ 2048Console/2048.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "Functions.h"
+
+using namespace std;
+
+int main()
+{
+	int number;
+	cin >> number;
+	game(number);
+}
---
+++ 2048Console/Functions.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void game(int number);
